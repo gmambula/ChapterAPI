1ef10e193ec4573c10fc02d6dc300f410e70150a
@gmambula <gmambula@gmail.com>
2022-12-18 19:19:00 -0300

    commit encontro remoto 1

---
+++ ChapterAPI/ChapterAPI/Controllers/LivroController.cs
@@ -0,0 +1,35 @@
+﻿using ChapterAPI.Interfaces;
+using ChapterAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterAPI.Controllers
+{
+    [Produces("application/json")] 
+    [Route("api/[controller]")] //api/livro
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livro) 
+        {
+            _livroRepository = livro;
+        }
+
+        public IActionResult Listar() 
+        {
+            try
+            {
+                return Ok(_livroRepository.Ler());
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception (error.Message);
+            }
+        }
+
+    }
+}
